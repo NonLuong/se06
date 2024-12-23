490ccf04fc4ad91a09ab28ed05c8857613adab24
Final <Fennikzz1@example.com>
2024-12-23 19:08:38 +0700

    Promotion - close #25

--- backend/entity/Promotion.go
+++ backend/entity/Promotion.go
@@ -4,25 +4,27 @@ package entity
 import (
 	
 	"time"
+
 	"gorm.io/gorm"
 
 )
 
 type Promotion struct {
-    gorm.Model
-    PromotionCode        string         `json:"promotion_code" gorm:"unique;not null"`
-    PromotionName        string         `json:"promotion_name"`
-    PromotionDescription string         `json:"promotion_description"`
-    Discount             float64        `json:"discount"`
-    EndDate              time.Time      `json:"end_date"`
-    UseLimit             int            `json:"use_limit"`
-    UseCount             int            `json:"use_count"`
-    Distance             float64        `json:"distance"`
-    Photo                string         `gorm:"type:longtext" json:"photo"`
+	gorm.Model // gorm.Model จะทำให้มี ID, CreatedAt, UpdatedAt, DeletedAt อัตโนมัติ
 
-    DiscountTypeID       uint           `json:"discount_type_id"`
-    DiscountType         *DiscountType  `gorm:"foreignKey:DiscountTypeID" json:"discount_type"`
+	PromotionCode        string    `json:"promotion_code" gorm:"unique;not null"`      // รหัสโปรโมชั่น
+	PromotionName        string    `json:"promotion_name"`                              // ชื่อโปรโมชั่น
+	PromotionDescription string    `json:"promotion_description"`                       // คำอธิบายโปรโมชั่น
+	Discount             int   `json:"discount"`                                    // ส่วนลดโปรโมชั่น
+	EndDate              time.Time `json:"end_date"`                                    // วันที่หมดเขตโปรโมชั่น
+	UseLimit             int       `json:"use_limit"`                                 	// จำนวนครั้งที่สามารถใช้โค้ดได้
+	UseCount             int       `json:"use_count"`                                  	// จำนวนที่ใช้แล้ว
+	DistancePromotion    float64   `json:"distance_promotion"`                          // ระยะทางสูงสุด (กรอกเองในโปรโมชั่น)
+    Photo                string    `gorm:"type:longtext" json:"photo"`  				// รูปโปรโมชั่น
 
-    StatusPromotionID    uint           `json:"statuspromotion_id"`
-    StatusPromotion      *StatusPromotion `gorm:"foreignKey:StatusPromotionID" json:"statuspromotion"`
+	DiscountTypeID  	uint      		`json:"discount_type_id"`
+	DiscountType    	*DiscountType  	`gorm:"foreignKey: discount_type_id" json:"discount_type"`
+
+	StatusPromotionID  	uint      		`json:"status_promotion_id"`
+	StatusPromotion    	*StatusPromotion  `gorm:"foreignKey: status_promotion_id" json:"status_promotion"`
 }
